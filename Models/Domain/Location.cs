9b4aed791e3d2ed54f6f566ccc21bf5f82d001ce
jantubeeckx <jan.tubeeckx@student.hogent.be>
2020-12-18 11:08:53 +0100

    commit Add domain layer including unit tests

---
+++ Models/Domain/Location.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Beerhall.Models.Domain
+{
+    public class Location
+    {
+        public string PostalCode { get; set; }
+        public string Name { get; set; }
+    }
+}
