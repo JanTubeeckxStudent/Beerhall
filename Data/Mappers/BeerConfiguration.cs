1d0cc41b0715677f91b459fc5ada205102107117
jantubeeckx <jan.tubeeckx@student.hogent.be>
2020-12-18 19:02:01 +0100

    Configure EF and add databas initialization

---
+++ Data/Mappers/BeerConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Beerhall.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Beerhall.Data.Mappers
+{
+    public class BeerConfiguration
+    {
+        public class LocationConfiguration
+        {
+            public class BeerConfiguration : IEntityTypeConfiguration<Beer>
+            {
+                public void Configure(EntityTypeBuilder<Beer> builder)
+                {
+                    //Table name
+                    builder.ToTable("Beer");
+                    // Properties
+                    builder.Property(b => b.Name).IsRequired().HasMaxLength(100);
+                }
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Beerhall.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,10 +26,12 @@ namespace Beerhall
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<BeerhallDataInitializer>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BeerhallDataInitializer beerhallDataInitializer)
         {
             if (env.IsDevelopment())
             {
@@ -52,6 +56,8 @@ namespace Beerhall
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            beerhallDataInitializer.InitializeData();
         }
     }
 }
